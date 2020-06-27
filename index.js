380d3c37158ca4aa4f7a3e0080ce63c55043d7c6
sunil <sunilkumarbajpai.skb@gmail.com>
2020-06-27 19:21:49 +0530

    setup static folder

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express=require('express');
 const app=express();
 const port=8000;
 const expressLayouts=require('express-ejs-layouts');
-
+app.use(express.static('./assets'));
 app.use(expressLayouts);
 
 //use express router
