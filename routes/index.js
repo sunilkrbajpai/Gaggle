315fbf981190c5dbb75f56d3f3823e4704c6a31c
sunil <sunilkumarbajpai.skb@gmail.com>
2020-06-27 18:51:07 +0530

    setup controller

--- routes/index.js
+++ routes/index.js
@@ -1,3 +1,6 @@
 const express=require('express');
+const homeController=require('../controllers/home_controller');
 const router=express.Router();
+
+router.get('/',homeController.home);
 module.exports=router;
