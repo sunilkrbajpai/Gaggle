57b04019e0dbf69bc7644a867019203445900f35
sunil <sunilkumarbajpai.skb@gmail.com>
2020-06-29 00:51:49 +0530

    commented routes

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,9 @@
-const express=require('express');
-const homeController=require('../controllers/home_controller');
-const router=express.Router();
+const express=require('express');                                   //require express
+const homeController=require('../controllers/home_controller');     //require homecontrller
+const router=express.Router();                                      
 
-router.get('/',homeController.home);
-router.use('/users',require('./users'));
+router.get('/',homeController.home);                                //send to home 
+router.use('/users',require('./users'));                            //if URL has /users then route to ./users
+
+// export
 module.exports=router;
--- routes/users.js
+++ routes/users.js
@@ -1,17 +1,20 @@
-const express=require('express');
+const express=require('express');                   //require express
 const router=express.Router();
-const passport=require('passport');
-const userController=require('../controllers/users_controller');
+const passport=require('passport');                 //require passport
+const userController=require('../controllers/users_controller'); //require usercontroller
 
-router.get('/profile',passport.checkAuth,userController.profile);
-router.get('/sign-up',userController.signUp);
-router.get('/sign-in',userController.signIn);
-router.post('/create',userController.create);
-router.post('/changePwd',userController.changePwd);
-router.get('/forgot-password',userController.forgotPassword);
-router.post('/create-session',passport.authenticate('local',{failureRedirect:'/users/sign-in'}),userController.createSession);
-router.get('/sign-out',userController.destroy);
-router.get('/auth/google',passport.authenticate('google',{scope:['profile','email']}));
-router.get('/auth/google/callback',passport.authenticate('google',{failureRedirect:'/users/sign-in'}),userController.createSession);
-router.post('/reset-password',userController.reset);
+router.get('/profile',passport.checkAuth,userController.profile);         //send to profile page
+router.get('/sign-up',userController.signUp);                              //send to sign up page
+router.get('/sign-in',userController.signIn);                             //send to sign in page
+router.post('/create',userController.create);                             //post the data to create user
+router.post('/changePwd',userController.changePwd);                       //post the data to change password
+router.get('/forgot-password',userController.forgotPassword);             //render forgot password
+router.post('/create-session',passport.authenticate('local',{failureRedirect:'/users/sign-in'}),userController.createSession);  //create session
+
+
+router.get('/sign-out',userController.destroy);                            //send to sign out
+router.get('/auth/google',passport.authenticate('google',{scope:['profile','email']}));     //send to google authentication page
+router.get('/auth/google/callback',passport.authenticate('google',{failureRedirect:'/users/sign-in'}),userController.createSession);   //send back to home
+router.post('/reset-password',userController.reset);            //post the reset link
+
 module.exports=router;   //router exported
