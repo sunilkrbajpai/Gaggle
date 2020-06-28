606dc3b23666c80cc63eeb665b85ced43efe87af
sunil <sunilkumarbajpai.skb@gmail.com>
2020-06-29 00:42:15 +0530

    commented controllers

--- assets/js/sign-in.js
+++ assets/js/sign-in.js
@@ -1,6 +1,8 @@
 $(document).ready(function(){
     $("#submitbtn").click(function(){  
             var recaptcha=$("#g-recaptcha-response").val();
+
+            //if recaptcha is not clicked
             if(recaptcha===""){
                     event.preventDefault();
                     alert("Please fill reCaptcha!");
--- controllers/users_controller.js
+++ controllers/users_controller.js
@@ -1,11 +1,11 @@
-const User=require('../models/user');
-const crypto=require('crypto');
-const resetMailers=require('../mailers/reset_mailer');
-const loginMailer=require('../mailers/login_mailers');
-const bcrypt = require('bcrypt');
+const User=require('../models/user');                   //require user
+const crypto=require('crypto');                         //require crypto
+const resetMailers=require('../mailers/reset_mailer');  //require resetMailers to send mail
+const loginMailer=require('../mailers/login_mailers'); //require login_Mailers to send mail
+const bcrypt = require('bcrypt');                           //require bcrypt to encrypt
 const saltRounds = 10;
 
-
+// render profile page
 module.exports.profile=function(req,res){
     return res.render('user_profile',{
         title:'User Profile'
@@ -46,22 +46,26 @@ module.exports.signIn=function(req,res){
 // get the sign up data
 module.exports.create=function(req,res)
 {
-    console.log(req.body);
+    // console.log(req.body);
 
+    // if password and confirm_pass dont match
     if(req.body.password!=req.body.confirm_pwd)
     {
         req.flash('error','Password and confirm password should match!')
         return res.redirect('back');
     }
+
     User.findOne({email:req.body.email},function(err,user){
         if(err){console.log('Error in finding the user while sign up');return;}
 
+        // if user not found then create new user
         if(!user){
 
             const name=req.body.name;
             const email=req.body.email;
             const password=req.body.password;
 
+            // encrypt password and save
             bcrypt.hash(password, saltRounds, function(err, hash) {
                 let a={email:email,password:hash,name:name};
                 User.create(a,function(err,user){
@@ -78,12 +82,14 @@ module.exports.create=function(req,res)
     })
 }
 
+// sign out code
 module.exports.destroy=function(req,res){
     req.logout();
     req.flash('success','Signed out successfully!')
     return res.redirect('/');
 }
 
+// when change password in profile page
 module.exports.changePwd=function(req,res)
 {
     if(req.body.new_pass!=req.body.confirm_pass)
@@ -92,6 +98,7 @@ module.exports.changePwd=function(req,res)
         return res.redirect('back');
     }
 
+    // use bcrypt to compare between plaintext from input and encrypted password in DB
     bcrypt.compare(req.body.old_pass, req.user.password, function(err, result) {
         // result == true
         if(!result)
@@ -120,11 +127,14 @@ module.exports.changePwd=function(req,res)
 
 }
 
+// render forgotPassword page
 module.exports.forgotPassword=function(req,res){
     return res.render('forgot_password',{
         title:'Gaggle | Forgot password'
     });
 }
+
+// send reset password to mail
 module.exports.reset=function(req,res){
     
     // console.log(req.body.email);
