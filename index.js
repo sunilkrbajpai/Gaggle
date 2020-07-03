6455ea9b646e8846c37b26279475d080e429cea9
Sunil kumar bajpai <sunilkumarbajpai.skb@gmail.com>
2020-07-03 15:23:51 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 const express=require('express');
 const app=express();
-const port=8000;
+const port=process.env.PORT || 8000;
 const expressLayouts=require('express-ejs-layouts');
 const db=require('./config/mongoose');
 //used for session cookie
